7eb0fd6fd44f0d74d9a3b6d06c50e9d0934cdba3
Tamas Ruszka <ruszka.tamas@gmail.com>
2024-04-02 19:46:15 +0200

    Add default style elements

--- src/react/pages/dashboard.tsx
+++ src/react/pages/dashboard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const DashboardPage = () => {
-	return <div>Dashboard</div>;
+	return <div className='dashboard-page'>Dashboard</div>;
 };
 
 export default DashboardPage;
