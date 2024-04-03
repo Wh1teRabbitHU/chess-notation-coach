35fc67e701bfdb169849cab9211135940e7fb5ed
Tamas Ruszka <ruszka.tamas@gmail.com>
2024-04-03 20:42:25 +0200

    Test pre commit hook

--- src/react/pages/dashboard.tsx
+++ src/react/pages/dashboard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const DashboardPage = () => {
-	return <div className='dashboard-page'>Dashboard</div>;
+	return <div className='dashboard-page'>Chess Notation Coach - Dashboard</div>;
 };
 
 export default DashboardPage;
