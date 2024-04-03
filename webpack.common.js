dcc971c6d640cd34f1735dda19e19a2bb2a7767e
Tamas Ruszka <ruszka.tamas@gmail.com>
2024-04-03 21:36:38 +0200

    Fixing the development script

--- webpack.common.js
+++ webpack.common.js
@@ -1,19 +1,11 @@
-import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import CopyWebpackPlugin from 'copy-webpack-plugin';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
-
-import { __dirname, packageJson } from './node-env.js';
 
 export default {
 	entry: './src/react/main.tsx',
 	resolve: {
 		extensions: ['.js', '.jsx', '.ts', '.tsx']
-	},
-	output: {
-		filename: `${packageJson.name}-${packageJson.version}.bundle.js`,
-		path: path.resolve(__dirname, './docs'),
-		clean: true
 	},
 	module: {
 		rules: [
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,17 @@
+import path from 'path';
 import mergeTool from 'webpack-merge';
 import common from './webpack.common.js';
+
+import { __dirname, packageJson } from './node-env.js';
 
 export default function webpackConfig() {
 	return mergeTool.merge(common, {
 		mode: 'production',
+		output: {
+			filename: `${packageJson.name}-${packageJson.version}.bundle.js`,
+			path: path.resolve(__dirname, './docs'),
+			clean: true
+		},
 		optimization: {
 			minimize: true
 		}
